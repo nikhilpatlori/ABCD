e2299359df4775506204815cc4dc19c174bcfbb4
testengineer038 <testengineer038@gmail.com>
2022-08-04 15:51:14 +0200

    adding to github

---
+++ src/test/java/basicTest/PrintProgram.java
@@ -0,0 +1,25 @@
+package basicTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PrintProgram {
+
+	@Test
+	public void print() {
+		System.out.println("Hello World");
+	}
+	
+	@Test
+	public void selbrowlaunch() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https:www.google.com");
+	}
+		
+
+
+}
